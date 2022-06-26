f355caafda38af70490c9fbe55c51a55e7918e1c
taKKen <takgeun92@gmail.com>
2022-06-27 03:51:20 +0900

    first

---
+++ 백업/20220627/Websocketchat03/src/main/java/com/websocket/chat/Websocketchat03Application.java
@@ -0,0 +1,13 @@
+package com.websocket.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocketchat03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocketchat03Application.class, args);
+	}
+
+}
