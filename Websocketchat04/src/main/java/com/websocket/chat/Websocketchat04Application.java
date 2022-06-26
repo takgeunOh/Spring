a3937e3cce2bad110320f70f2b4673b80348e117
taKKen <takgeun92@gmail.com>
2022-06-27 03:36:36 +0900

    websocketchat04 first commit

---
+++ Websocketchat04/src/main/java/com/websocket/chat/Websocketchat04Application.java
@@ -0,0 +1,13 @@
+package com.websocket.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocketchat04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocketchat04Application.class, args);
+	}
+
+}
